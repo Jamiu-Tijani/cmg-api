67e576d64d8dc4500dfe1a477368b2c5473ccd95
Jamiu-Tijani <jimniz01@gmail.com>
2023-04-09 15:22:20 -0400

    fix urls

--- cmg_api/urls.py
+++ cmg_api/urls.py
@@ -18,6 +18,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-      path("v1/", include('accounts.urls'))
+      path("v1/", include('accounts.urls'),),
+      path("v1/", include('socials.urls'),)
 
 ]
